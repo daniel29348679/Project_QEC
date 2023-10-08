af930ba7b2310c64d3550a2ec3777046bdb59a90
Godkiller <daniel29348679@gmail.com>
2023-10-08 16:34:23 +0800

    2023/10/8

--- adder/circle_test.py
+++ adder/circle_test.py
@@ -119,7 +119,7 @@ for i in y1:
 
 y1 = []
 for i in x:
-    y1.append(testcircle_rand(6, i, corrrate))
+    y1.append(testcircle_rand(10, i, corrrate))
     print(y1[-1])
 plt.plot(x, y1, color="c", label="corr")
 
