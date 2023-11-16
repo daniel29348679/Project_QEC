f5b1b02a74b022d9a7db8d249fe5d81ed3198582
Godkiller <daniel29348679@gmail.com>
2023-11-16 22:18:46 +0800

    booltest d_p

--- adder/booltest.py
+++ adder/booltest.py
@@ -56,6 +56,9 @@ class boolcirc:
     def swap_direct(self, a, b):
         self.qubit[a], self.qubit[b] = self.qubit[b], self.qubit[a]
 
+    def permutation(self, a, b):
+        self.qubit[a:b] = np.random.permutation(self.qubit[a:b])
+
     def count(self, f, e, value):
         count = 0
         for i in range(f, e):
@@ -71,10 +74,6 @@ class boolcirc:
             else:
                 s += "0"
         return s
-
-
-prob_2 = 0.003  # 2-qubit gate 0.007395
-shots = 10000
 
 
 # %% Make a circuit
@@ -137,40 +136,132 @@ def testcircle_rand(numofqubit, totaltime, coorrate, makeerror, measurerate):
     return [i / shots for i in counts]
 
 
+def testcircle_rand_double(numofqubit, totaltime, coorrate, makeerror, measurerate):
+    counts = [shots % numofqubit] * (totaltime // measurerate + 1)
+    for _ in tqdm(range(shots // numofqubit)):
+        circ = boolcirc(numofqubit * 2, prob_2)
+        count = 0
+        for k in range(totaltime + 1):
+            if makeerror > 0:
+                for i in range(numofqubit):
+                    circ.checkerror(i, makeerror)
+            if k % coorrate == coorrate - 1:
+                for i in range(numofqubit):
+                    circ.reset(i + numofqubit)
+                    fulladder(
+                        circ,
+                        (i - 1 - count % 2) % numofqubit,
+                        i,
+                        (i + 1 + (count + 1) % 2) % numofqubit,
+                        i + numofqubit,
+                    )
+                for i in range(0, numofqubit, 2):
+                    circ.swap(i, (i + 1) % numofqubit)
+                for i in range(1, numofqubit, 2):
+                    circ.swap(i, (i + 1) % numofqubit)
+            for i in range(numofqubit):
+                circ.swap_direct(i + numofqubit, i)
+            if k % measurerate == 0:
+                counts[k // measurerate] += circ.count(0, numofqubit, False)
+    return [i / shots for i in counts]
+
+
+def testcircle_d_p(numofqubit, totaltime, coorrate, makeerror, measurerate):
+    counts = [shots % numofqubit] * (totaltime // measurerate + 1)
+    for _ in tqdm(range(shots // numofqubit)):
+        circ = boolcirc(numofqubit * 2, prob_2)
+        count = 0
+        for k in range(totaltime + 1):
+            if makeerror > 0:
+                for i in range(numofqubit):
+                    circ.checkerror(i, makeerror)
+            if k % coorrate == coorrate - 1:
+                for i in range(numofqubit):
+                    circ.reset(i + numofqubit)
+                    fulladder(
+                        circ,
+                        (i - 1) % numofqubit,
+                        i,
+                        (i + 1) % numofqubit,
+                        i + numofqubit,
+                    )
+            for i in range(numofqubit):
+                circ.swap_direct(i + numofqubit, i)
+            circ.permutation(0, numofqubit)
+            if k % measurerate == 0:
+                counts[k // measurerate] += circ.count(0, numofqubit, False)
+    return [i / shots for i in counts]
+
+
 # %%
+fig = plt.figure()
+fig.set_size_inches(16, 9)
+
+prob_2 = 0.002795  # 2-qubit gate 0.007395
+shots = 10000
 corrrate = 1  # 200
-measurerate = 10  # 200
-makeerror = 3
+measurerate = 5  # 200
+makeerror = 20
 totaltime = 100  # 2000
 x = range(0, totaltime + 1, measurerate)
 
 allsim = {}
 
-allsim["3bit"] = testcircle(3, totaltime, corrrate, makeerror, measurerate)
-print(f"3 bit = {allsim['3bit']}")
-plt.plot(x, allsim["3bit"], color="r", label="corr")
-
-allsim["6bit"] = testcircle(6, totaltime, corrrate, makeerror, measurerate)
-print(f"6 bit = {allsim['6bit']}")
-plt.plot(x, allsim["6bit"], color="r", label="corr")
-
 allsim["10bit_rand"] = testcircle_rand(10, totaltime, corrrate, makeerror, measurerate)
 print(f"10 bit_rand = {allsim['10bit_rand']}")
-plt.plot(x, allsim["10bit_rand"], color="c", label="corr")
+plt.plot(x, allsim["10bit_rand"], color="c", label="10 r")
 
-allsim["20bit_rand"] = testcircle_rand(20, totaltime, corrrate, makeerror, measurerate)
+allsim["20bit_rand"] = testcircle_rand(30, totaltime, corrrate, makeerror, measurerate)
 print(f"20 bit_rand = {allsim['20bit_rand']}")
-plt.plot(x, allsim["20bit_rand"], color="c", label="corr")
+plt.plot(x, allsim["20bit_rand"], color="c", label="20 r")
+
+allsim["10bit_rand_d"] = testcircle_rand_double(
+    10, totaltime, corrrate, makeerror, measurerate
+)
+print(f"10 bit_rand_d = {allsim['10bit_rand_d']}")
+plt.plot(x, allsim["10bit_rand_d"], color="m", label="10 rd")
+
+allsim["20bit_rand_d"] = testcircle_rand_double(
+    20, totaltime, corrrate, makeerror, measurerate
+)
+print(f"20 bit_rand_d = {allsim['20bit_rand_d']}")
+plt.plot(x, allsim["20bit_rand_d"], color="m", label="20 rd")
+
+allsim["10bit_d_p"] = testcircle_d_p(10, totaltime, corrrate, makeerror, measurerate)
+print(f"10 bit_d_p = {allsim['10bit_d_p']}")
+plt.plot(x, allsim["10bit_d_p"], color="gray", label="10 dp")
+
+allsim["20bit_d_p"] = testcircle_d_p(20, totaltime, corrrate, makeerror, measurerate)
+print(f"20 bit_d_p = {allsim['20bit_d_p']}")
+plt.plot(x, allsim["20bit_d_p"], color="gray", label="20 dp")
+
+allsim["40bit_d_p"] = testcircle_d_p(40, totaltime, corrrate, makeerror, measurerate)
+print(f"40 bit_d_p = {allsim['40bit_d_p']}")
+plt.plot(x, allsim["40bit_d_p"], color="gray", label="40 dp")
 
 allsim["base"] = testcircle(6, totaltime, corrrate, makeerror, measurerate)
 print(f"base = {allsim['base']}")
-plt.plot(x, allsim["base"], color="r", label="no corr")
-
-plt.title(f"correct per {corrrate} x gates prob_2={prob_2}")  # title
+plt.plot(x, allsim["base"], color="g", label="no corr")
+plt.title(f"correct per {makeerror} x gates prob_2={prob_2}")  # title
 plt.ylabel("correct rate")  # y label
-plt.xlabel("x gates")  # x label
+plt.xlabel(f"{makeerror}x CX gates")  # x label
+plt.legend()  # show legend
+# %%
+fig = plt.figure()
+fig.set_size_inches(16, 9)
+plt.plot(x, allsim["10bit_rand"], color="c", label="10 r")
+plt.plot(x, allsim["20bit_rand"], color="c", label="20 r")
+plt.plot(x, allsim["10bit_rand_d"], color="m", label="10 rd")
+plt.plot(x, allsim["20bit_rand_d"], color="m", label="20 rd")
+plt.plot(x, allsim["10bit_d_p"], color="gray", label="10 dp")
+plt.plot(x, allsim["20bit_d_p"], color="gray", label="20 dp")
+plt.plot(x, allsim["40bit_d_p"], color="gray", label="40 dp")
+plt.plot(x, allsim["base"], color="g", label="no corr")
 
-plt.show()
 
+plt.title(f"correct per {makeerror} x gates prob_2={prob_2}")  # title
+plt.ylabel("correct rate")  # y label
+plt.xlabel(f"{makeerror}x CX gates")  # x label
+plt.legend()  # show legend
 
 # %%
